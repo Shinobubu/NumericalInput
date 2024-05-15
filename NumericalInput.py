a4df7e6680f1c550392006a7d60df73b5888b24f
Shinobubu <shinobu.bu@gmail.com>
2024-05-15 01:30:44 -0700

    Update NumericalInput.py add abort
    putting in a blank field will abort operation.

--- NumericalInput.py
+++ NumericalInput.py
@@ -109,7 +109,12 @@ class NumericalInput():
 			values = args[0]	
 			# remove white spaces
 			values = values.strip()
-			values = values.split(" ")					
+			values = values.split(" ")		
+
+			if values == "":
+				if(cmds.window("InputPrompt",q=1,ex=1)):
+					cmds.evalDeferred(lambda: cmds.deleteUI("InputPrompt"))
+
 			x,y,z = (0 , 0 , 0)
 			v = values[0] # for single only entries
 			if len(values) > 1:		
@@ -289,7 +294,12 @@ class NumericalInput():
 			values = args[0]	
 			# remove white spaces
 			values = values.strip()
-			values = values.split(" ")					
+			values = values.split(" ")	
+			
+			if values == "":
+				if(cmds.window("InputPrompt",q=1,ex=1)):
+					cmds.evalDeferred(lambda: cmds.deleteUI("InputPrompt"))
+
 			x,y,z = (0 , 0 , 0)
 			v = values[0] # for single only entries
 			if len(values) > 1:		
